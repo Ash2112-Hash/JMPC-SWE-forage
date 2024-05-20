9610dd54a54f8af268d43c3f1160b265a47c60a4
Ashwin Unnithan <unnithanash@gmail.com>
2024-05-20 14:37:54 -0400

    Added Unit Tests for client - Task 1

--- client_test.py
+++ client_test.py
@@ -1,5 +1,5 @@
 import unittest
-from client3 import getDataPoint
+from client3 import getDataPoint, getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -8,6 +8,8 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    self.assertEqual(getDataPoint(quotes[0]), ('ABC', 120.48, 121.2, ((120.48+121.2)/2)))
+    self.assertEqual(getDataPoint(quotes[1]), ('DEF', 117.87, 121.68, ((117.87 + 121.68) / 2)))
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,6 +17,15 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    self.assertEqual(getDataPoint(quotes[0]), ('ABC', 120.48, 119.2, ((120.48 + 119.2) / 2)))
+    self.assertEqual(getDataPoint(quotes[1]), ('DEF', 117.87, 121.68, ((117.87 + 121.68) / 2)))
+
+  def test_getRatio_normal(self):
+    self.assertEqual(getRatio(33, 55), 0.6)
+
+  def test_getRatio_divisionbyZero(self):
+    getRatio(33, 0)
+    self.assertRaises(ZeroDivisionError)
 
 
   """ ------------ Add more unit tests ------------ """
